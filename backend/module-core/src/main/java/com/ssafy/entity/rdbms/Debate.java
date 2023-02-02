a656fa3770c4536b7019bd4fd23b6ca46c71ba95
임하림 <gkgid0840@naver.com>
2023-02-03 00:15:38 +0900

    Fix : Build 오류 해결 - Jenkins 배포 환경 테스트

--- backend/module-core/src/main/java/com/ssafy/entity/rdbms/Debate.java
+++ backend/module-core/src/main/java/com/ssafy/entity/rdbms/Debate.java
@@ -1,9 +1,6 @@
 package com.ssafy.entity.rdbms;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -13,6 +10,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Debate extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
