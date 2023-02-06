92d33919502e4933b3090d5fec12ce19a4f9b7e2
PandaMun <61276800+Muntaeho@users.noreply.github.com>
2023-02-06 09:52:15 +0900

    commit

--- backend/module-chat/src/main/java/com/ssafy/modulechat/model/KafkaConstants.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.modulechat.model;
-
-public class KafkaConstants {
-    public static final String KAFKA_TOPIC = "kafka-chat";
-    public static final String GROUP_ID = "foo";
-    public static final String KAFKA_BROKER = "i8c205.p.ssafy.io:9092";
-}
