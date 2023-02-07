c2ff0375f44ba1b3adc4e22809c454bc5454a554
문태호 <ansxogh97@gmail.com>
2023-02-07 15:14:32 +0900

    Feat : faq frontPage

--- frontend_tem/src/views/pages/pages/faq.vue
+++ frontend_tem/src/views/pages/pages/faq.vue
@@ -1,119 +1,134 @@
 <template>
     <!-- Main Wrapper -->
     <div class="main-wrapper">
-        
+
         <layoutsloginborder></layoutsloginborder>
-       
+
             <faqbreadcrumb></faqbreadcrumb>
-			
+
 			<!-- Help Details -->
 			<div class="help-sec">
-				
+
 				<div class="container">
 					<div class="row">
 						<div class="col-lg-6">
 							<div class="help-title">
-								<h1>Most frequently asked questions</h1>
-								<p>Here are the most frequently asked questions you may check before getting started</p>
+								<h1>FAQ</h1>
+								<p>자주 묻는 질문</p>
 							</div>
 						</div>
 					</div>
-					
-					<div class="row">
-						<div class="col-lg-6">
-						
-							<!-- Faq -->							
-							<div class="faq-card">
+          <div class="category-tab">
+            <ul class="nav nav-justified">
+              <li class="nav-item" ><a class="nav-link" data-bs-toggle="tab" @click="changeCategory('회원 기능')">회원 기능</a></li>
+              <li class="nav-item" ><a class="nav-link" data-bs-toggle="tab" @click="changeCategory('토론 기능')">토론 기능</a></li>
+              <li class="nav-item" ><a class="nav-link" data-bs-toggle="tab" @click="changeCategory('증명서 발급 기능')">증명서 발급 기능</a></li>
+            </ul>
+          </div>
+
+
+							<div class="faq-card" v-for="(content,index) in contents" >
 								<h6 class="faq-title">
-									<a class="collapsed" data-bs-toggle="collapse" href="#faqone" aria-expanded="false">Is there a 14-days trial?</a>
+									<a class="collapsed" data-bs-toggle="collapse" :href="'#faq'+index" aria-expanded="false">{{content.content}}</a>
 								</h6>
-								<div id="faqone" class="collapse">
+								<div :id="'faq'+index" class="collapse">
 									<div class="faq-detail">
-										<p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.</p>
-										<p>If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages.</p>
+										<p>{{content.comment}}</p>
 									</div>
 								</div>
 							</div>
-							
-														
-							<div class="faq-card">
-								<h6 class="faq-title">
-									<a class="collapsed" data-bs-toggle="collapse" href="#faqtwo" aria-expanded="false">How much time I will need to learn this app?</a>
-								</h6>
-								<div id="faqtwo" class="collapse">
-									<div class="faq-detail">
-										<p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.</p>
-										<p>If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages.</p>
-									</div>
-								</div>
-							</div>
-							
-														
-							<div class="faq-card">
-								<h6 class="faq-title">
-									<a class="collapsed" data-bs-toggle="collapse" href="#faqthree" aria-expanded="false">Is there a month-to-month payment option?</a>
-								</h6>
-								<div id="faqthree" class="collapse">
-									<div class="faq-detail">
-										<p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.</p>
-										<p>If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages.</p>
-									</div>
-								</div>
-							</div>
-							<!-- /Faq -->
-						
-						</div>	
-						
-						<div class="col-lg-6">
-						
-							<!-- Faq -->							
-							<div class="faq-card">
-								<h6 class="faq-title">
-									<a class="collapsed" data-bs-toggle="collapse" href="#faqfour" aria-expanded="false">What’s the benefits of the Premium Membership?</a>
-								</h6>
-								<div id="faqfour" class="collapse">
-									<div class="faq-detail">
-										<p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.</p>
-										<p>If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages.</p>
-									</div>
-								</div>
-							</div>
-							
-														
-							<div class="faq-card">
-								<h6 class="faq-title">
-									<a class="collapsed" data-bs-toggle="collapse" href="#faqfive" aria-expanded="false">Are there any free tutorials available?</a>
-								</h6>
-								<div id="faqfive" class="collapse">
-									<div class="faq-detail">
-										<p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.</p>
-										<p>If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages.</p>
-									</div>
-								</div>
-							</div>
-							
-														
-							<div class="faq-card">
-								<h6 class="faq-title">
-									<a class="collapsed" data-bs-toggle="collapse" href="#faqsix" aria-expanded="false">How can I cancel my subscription plan?</a>
-								</h6>
-								<div id="faqsix" class="collapse">
-									<div class="faq-detail">
-										<p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.</p>
-										<p>If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages. The new common language will be more simple and regular than the existing European languages.</p>
-									</div>
-								</div>
-							</div>
-							<!-- /Faq -->
-						
-						</div>	
-					</div>
-				</div>
-			</div>
-			<!-- /Help Details -->
-        
+
+          <div class="container" style="text-align: center;">
+            <div class="row">
+              <div class="col-md-12">
+                <ul class="pagination lms-page">
+                  <li v-for="index in totalPages" class="page-item first-page active">
+                    <a class="page-link" @click="fetchData(index,10)">{{index}}</a>
+                  </li>
+
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /Help Details -->
+
         <layouts1></layouts1>
-       
+
     </div>
     <!-- /Main Wrapper -->
 </template>
+<script>
+import { apiInstance } from "../../../api/index.js";
+
+export default {
+
+  data() {
+
+    return {
+      category : '회원 기능',
+      contents: [],
+      pageable: {
+        number: 0,
+        size: 10,
+      },
+      totalElements: 0,
+      totalPages: 0
+    };
+  },
+  computed: {
+
+    canPrev() {
+      return this.page.number > 0;
+    },
+    canNext() {
+      return this.page.number < this.page.totalPages - 1;
+    }
+  },
+  methods: {
+    prevPage() {
+      if (this.canPrev) {
+        this.fetchData(this.page.number - 1, this.page.size);
+      }
+    },
+    nextPage() {
+      if (this.canNext) {
+        this.fetchData(this.page.number + 1, this.page.size);
+      }
+    },
+    changeCategory(category){
+      this.category = category;
+      this.fetchData(0,10);
+    },
+    async fetchData(pageNumber, pageSize) {
+      const api = apiInstance();
+      await api.get("/faq/list/"+this.category, { params: { page: pageNumber-1, size: pageSize } })
+          .then(response => {
+            this.contents = response.data.content;
+            this.pageable = response.data.pageable;
+            this.last = response.data.last;
+            this.totalPages = response.data.totalPages;
+            console.log(response.data)
+          });
+    }
+  },
+  mounted() {
+    this.fetchData(1, 10);
+  }
+
+}
+
+</script>
+
+
+<!--<template>-->
+<!--  <div>-->
+<!--    <button @click="prevPage" :disabled="!canPrev">Previous</button>-->
+<!--    <button @click="nextPage" :disabled="!canNext">Next</button>-->
+<!--    <ul>-->
+<!--      <li v-for="item in items" :key="item.id">{{ item.name }}</li>-->
+
+<!--    </ul>-->
+<!--  </div>-->
+<!--</template>-->
