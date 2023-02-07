b1e72fe59d2758556d58cbd5705f49a63a813594
홍예진 <teepij@naver.com>
2023-02-07 15:29:22 +0900

    Feat : 평가 문항 조회 API 연결 완료. 평가 생성시 대상 라디오 버튼 비활성화 추가

---
+++ backend/module-api/src/main/java/com/ssafy/api/service/CommonCodeService.java
@@ -0,0 +1,9 @@
+package com.ssafy.api.service;
+
+import com.ssafy.entity.rdbms.CommonCode;
+
+import java.util.List;
+
+public interface CommonCodeService {
+    CommonCode getQuestionList();
+}
--- backend/module-core/src/main/java/com/ssafy/entity/rdbms/CommonCode.java
+++ backend/module-core/src/main/java/com/ssafy/entity/rdbms/CommonCode.java
@@ -1,5 +1,6 @@
 package com.ssafy.entity.rdbms;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
@@ -10,17 +11,21 @@ import java.util.List;
 
 @Entity
 @Getter
-@ToString
+@ToString(exclude = "parent")
 @EqualsAndHashCode(callSuper = true)
 public class CommonCode extends BaseEntity{
+
+    private Long id;
 
     @Column
     private String codeName;
 
+    @JsonIgnore
     private String description;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id", referencedColumnName = "id", insertable = false, updatable = false)
+    @JsonIgnore
     private CommonCode parent;
 
     @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY)
